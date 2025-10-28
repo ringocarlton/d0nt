eac9426ca314b7503a7aae97bdf52b03b127deed
ringocarlton <chorumz@gmail.com>
2025-10-28 22:07:07 +0200

    Delete generate-images-json.js

--- generate-images-json.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = './images/'; // folder with your images
-
-const files = fs.readdirSync(path);
-const images = files.filter(file => /\.(jpg|jpeg|png|gif|webp)$/i.test(file));
-fs.writeFileSync('images.json', JSON.stringify(images, null, 2));
-console.log('✅ images.json updated with', images.length, 'images');
